75ad2c74f2c16825aa8f2c4b14bed944334abd1e
Ricardo Alves <theopqam@protonmail.com>
2023-11-29 13:04:26 +0000

    Update autoPull.sh
    Added some necessary notes.
    
    This script is old and not how I'd do right now.
    Will update at a later date. Still need to try stuff with different environment config files

--- BashStuff/autoPull.sh
+++ BashStuff/autoPull.sh
@@ -14,17 +14,17 @@ current_date=$(date '+%Y-%m-%d %H:%M:%S')
 echo "[$current_date] Log entry" >> "$bad_log_file"
 echo "[$current_date] Log entry" >> "$good_log_file"
 
-# Give a couple of seconds for the system to be able to deal with the SSH agent
+# Give a couple of seconds for the system to be able to deal with the SSH agent (note: this wasn't necessary. But I keep it here for memory's sake)
 sleep 2
 
-# Appending the logs
+# Appending the logs (note: these are a serious issue if 'sourcing' the script instead of running it normally - infinite loop (why?). Do without if sourcing)
 exec 2>> "$bad_log_file"
 exec 1>> "$good_log_file"
 
 # Start the SSH agent
 eval $(ssh-agent -s)
 
-# Add SSH key to the agent (this is an example. Add correct key)
+# Add SSH key to the agent (this is an example. Add the correct key. Also, Verbosity can be added - and the logging removed or 'tee'ed' into the terminal if you want to read the info)
 ssh-add /root/.ssh/id_rsa
 
 
