c03819c9b2e240429a32479047d6a3d3851f1db5
OPQAM <theopqam@protonmail.com>
2024-05-06 23:57:18 +0100

    Feature: Another detour from the video on Bash. Cron jobs, tty, shells and subshells.
    Objective: More Bash stuffsesses

--- BashStuff/YetAnotherCourse/readme.md
+++ BashStuff/YetAnotherCourse/readme.md
@@ -4,4 +4,7 @@ Yes, yet another small course.
 Let's be real, I need to develop Bash, and I have little to no practice in it, so every excuse is good.
 Let's get on to it.
 
+-> Very(!) losely following along CODE IS EVERYTHING's video (Become a Bash Script Pro).
+I think this is the right attitude, since there isn't a real project behind my actions. I just want to tinker with shit and learn along.
+
 **snip**
--- BashStuff/YetAnotherCourse/yet_02_variables.sh
+++ BashStuff/YetAnotherCourse/yet_02_variables.sh
@@ -15,3 +15,21 @@ echo "Hello, ${name_-"Anonymous"}!"
 # Capturing the result of a command:
 # Say we wanted to have a variable obtain the actual result of the command tty. The way to achieve this is to place said command within brackets, like thus:
 . my_variable=$(tty)
+
+# Again, seriously sidetracking here, but I tried running something like this in crontab, with the intent of capturing the tty of the cron job as it is running (and then send whatever output to the current terminal - a dirty job). But it doesn't work. The cron job isn't actually running in another terminal, merely another subshell. And thus, the result of the tty capture is 'not a tty'. Indeed.
+#
+# If possible, move stuff like this to docs or something.
+#
+# By the way, this was the (nasty) cron job:
+
+* * * * * my_var=$(/usr/bin/tty) ; echo $my_var > /dev/pts/2
+
+# To clarify, the full path is just making sure that the tty is loaded, as cron might be running with a very limited set of tools. And /dev/pts/2 is my actual and current terminal.
+# FYI, as I'm writing, my other tmux terminal is being populated with 'not a tty'. Time to shut down that cron job.
+
+# Also, FYI, the current tmux terminal is /dev/pts/1
+
+# WIP
+--snip--
+
+
