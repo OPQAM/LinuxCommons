2ff9252abfa17bef523fcbeff5bf3e97eca0ad3d
opqam <alvesricardo@mail.com>
2024-05-08 17:58:46 +0100

    Feature: Added a couple of files - following video (basics)
    Objective: More Bash basics

---
+++ BashStuff/YetAnotherCourse/yet_04_userinput.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Note: instead of just making do with $1, $2, $3 as basic command line arguments,
+# we can instead give them adequate variable names, such as:
+movie_title="$1"
+movie_director="$2"
+genre="$3
+
+Command:
+
+xxxxx xxxxxxxx "
+
