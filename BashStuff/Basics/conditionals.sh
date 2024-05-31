dbe0a322b6afed9a1ae0b34d93b89dc721e6f4e9
OPQAM <theopqam@protonmail.com>
2024-05-31 14:23:57 +0100

    Doc: .md on a popu up idea for my Lazy scripts.
    Objective: Rough sketch for production

--- BashStuff/Basics/conditionals.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# We can use the logical AND or OR with '-a' or '-o':
-
-#ex:
-
-val=3
-
-[[  ]] 
--- BashStuff/YetAnotherCourse/yet_03_variables.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash 
-
-# Process substitution
-
-# diff <(ls ./v1) <(ls ./v2)
-
-# Arithmetic operations
-
-$((4 + 6))
